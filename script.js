6d8dccd5d669e825c311a04d25bf0ca050fbc3ca
ZaHiD-HaSaN-72 <jahidhasan19369@gmail.com>
2023-10-12 16:59:01 +0600

    add modal

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const ApiUrl = ('studenInfo.json');
 const selectStudent = document.getElementById('selectStudent');
 const StudentInfo = document.getElementById('studentInfo-box');
+const modalBox = document.getElementById('modalBox');
+let modalContent = document.getElementById('modalContent');
+let Closebtn = document.getElementById('popupClose');
+
 
 fetch(ApiUrl)
 .then(response => response.json())
@@ -11,27 +15,30 @@ fetch(ApiUrl)
          option.textContent = data.name;
          selectStudent.appendChild(option);
     }
-})
+});
 
 function StudentAllInfo(studentId){
     fetch(`${ApiUrl}`)
     .then(response =>  response.json())
     .then(data => {
-        
-
-        const student = data.find(c => c.id == studentId);
+        const student = data.find(s => s.id == studentId);
         console.log(student);
         let studentData =  `
-        
+        <img class="studentImg" src="${student.img}" alt="Photo Nai">
         <h2>${student.name}</h2>
+        <button  onclick="showModal()">See details</button>
+        `;
+        StudentInfo.innerHTML = studentData;
+
+        //modalcontent
+         let studentDetails = `
+         <p>Name: ${student.name}</p>
+         <p>Id Number: ${student.id}</p>
         <p>Age: ${student.age}</p>
         <p>Department: ${student.major}</p>
         <p>CGPA: ${student.grade}</p>
-        <img class="studentImg" src="${student.img}" alt="Photo Nai">
-         
         `;
-
-        StudentInfo.innerHTML = studentData;
+        modalContent.innerHTML = studentDetails;
     })
 };
 selectStudent.addEventListener('change', function(){
@@ -39,3 +46,11 @@ selectStudent.addEventListener('change', function(){
     StudentAllInfo(selectStudentId);
 }
 );
+function showModal(){
+    modalBox.style.display="block";
+}
+Closebtn.addEventListener('click',function(){
+    modalBox.style.display = 'none';
+});
+
+
